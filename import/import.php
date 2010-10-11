19f36c91985add52cc9839cdd002e99eafc95011
Tyler Bell <tylerwbell@gmail.com>
2010-10-11 14:36:00 -0700

    removed commented import lines

--- import/import.php
+++ import/import.php
@@ -5,15 +5,15 @@
  * (2) Download geoplanet data from http://developer.yahoo.com/geo/geoplanet/data/
  * (3) Add file names to the file variables below
  * (4) Run this script from the command line, i.e. "php import.php"
- * Status will be echoed
- * Script takes several hours
- * ensure that mysql has access to a multi-gigabyte temp dir
  * 
  * @package gplplanet
  * @author Tyler Bell tylerwbell[at]gmail[dot]com
  * @copyright (C) 2009,2010 - Tyler Bell
  * @license GNU General Public License
- * Ensure php.ini memory_limit is set to 128MB for handling large arrays of values (64MB not enough)
+ * @todo Check files exist
+ * @todo: Make easier to change database name
+ * @todo: create database programatically
+ * Ensure php.ini memory_limit is set to 1GB for handling large arrays
  * Temp tables require 50 GB
  */
 
@@ -25,12 +25,11 @@ $adjacenciesFile = "/tmp/geoplanet_adjacencies_7.5.2.tsv";
 error_reporting(E_ERROR);
 
 //==================== Usually no need to edit below this line =================
-set_time_limit(0);																//this script takes some time
+set_time_limit(0);															
 echo "Importing Yahoo Geoplanet Data\n";
 require_once ('class.geoimport.php');
 $importEngine = new geoimport;
 
-/*
 if (!$importEngine->importAdjacencies($adjacenciesFile)){exit;}
 if (!$importEngine->importPlaces($placesFile)){exit;}
 if (!$importEngine->importAliases($aliasFile)){exit;}
@@ -39,10 +38,7 @@ if (!$importEngine->populatePlaceNames()){exit;}
 if (!$importEngine->populateAdjacencies()){exit;}
 if (!$importEngine->populateParents()){exit;}
 if (!$importEngine->populateChildren()){exit;}
-
-*/
-
 if (!$importEngine->populateAncestors()){exit;}
+if (!$importEngine->populateDescendants()){exit;}
 
-//if (!$importEngine->populateDescendants()){exit;}
-
+echo "Import complete\n";
