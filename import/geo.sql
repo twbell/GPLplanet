eb6217a563c6d2d69af1548c5a4e79ffe0743e8f
Tyler Bell <tylerwbell@gmail.com>
2010-10-13 16:48:38 -0700

    - removed explicit db name - remvoed lock tables statement

--- import/geo.sql
+++ import/geo.sql
@@ -2,11 +2,9 @@
 -- Copyright (c) Tyler Bell 2009,2010 tylerwbell[at]gmail[dot]com
 
 --
--- Create geo database
+-- Designed to be loaded from import script, if used independently, create database then:
+-- mysql -u username -p databasename < dumpfile    
 --
-
-CREATE DATABASE IF NOT EXISTS `geo`;
-USE `geo`;
 
 --
 -- Create table `cache_names`
@@ -185,11 +183,8 @@ CREATE TABLE `raw_places` (
   KEY `parent_idx` (`parent`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Raw import table; can be deleted when import complete';
 
-
 ---
 --- Add Placetype Data to placetypes
 ---
-LOCK TABLES `geo_placetypes` WRITE;
 INSERT INTO `geo_placetypes` VALUES (6,"Street","A street","Street"),(7,"Town","A populated settlement such as a city, town, village","Town"),(8,"State","One of the primary administrative areas within a country","State"),(9,"County","One of the secondary administrative areas within a country","County"),(10,"Local Administrative Area","One of the tertiary administrative areas within a country","LocalAdmin"),(11,"Postal Code","A partial or full postal code","Zip"),(12,"Country","One of the countries or dependent territories defined by the ISO 3166-1 standard","Country"),(13,"Island","An island","Island"),(14,"Airport","An airport","Airport"),(15,"Drainage","A water feature such as a river, canal, lake, bay, ocean","Drainage"),(16,"Land Feature","A land feature such as a park, mountain, beach","LandFeature"),(17,"Miscellaneous","A uncategorized place","Miscellaneous"),(18,"Nationality","An area affiliated with a nationality","Nationality"),(19,"Supername","An area covering multiple countries","Supername"),(20,"Point of Interest","A point of interest such as a school, hospital, tourist attraction","POI"),(21,"Region","An area covering portions of several countries","Region"),(22,"Suburb","A subdivision of a town such as a suburb or neighborhood","Suburb"),(23,"Sports Team","A sports team","Sports Team"),(24,"Colloquial","A place known by a colloquial name","Colloquial"),(25,"Zone","An area known within a specific context such as MSA or area code","Zone"),(26,"Historical State","A historical primary administrative area within a country","HistoricalState"),(27,"Historical County","A historical secondary administrative area within a country","HistoricalCounty"),(29,"Continent","One of the major land masses on the Earth","Continent"),(31,"Time Zone","An area defined by the Olson standard (tz database)","Timezone"),(32,"Nearby Intersection","An intersection of streets that is nearby to the streets in a query string","Nearby Intersection"),(33,"Estate","A housing development or subdivision known by name","Estate"),(35,"Historical Town","A historical populated settlement that is no longer known by its original name","HistoricalTown"),(36,"Aggregate","An aggregate place","Aggregate"),(37,"Ocean","One of the five major bodies of water on the Earth","Ocean"),(38,"Sea","An area of open water smaller than an ocean","Sea");
-UNLOCK TABLES;
 
